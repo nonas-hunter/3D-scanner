fa57c0c2b167bd9d5d52270aae28640075132463
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-23 20:03:14 -0400

    Make file for scanner python code

---
+++ python/src/scanner.py
@@ -0,0 +1,11 @@
+class Scanner():
+    """API for interfacing with the scanner"""
+
+    def __init__(self):
+        pass
+
+    def set_position(self, theta_1, theta_2):
+        pass
+
+    def get_distance(self):
+        pass
