b38c406c49bb91e8d69503b709c85b2696687a94
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-27 18:53:31 -0400

    Restructure python package

--- python/scanviz/communication.py
+++ python/scanviz/communication.py
@@ -5,10 +5,10 @@ class Communication():
     """Infrastructure for serial communication with the Arduino."""
 
     EOM = "\r\n"
-    SEND_MESSAGE_TYPES = ["M", "S"]
+    SEND_MESSAGE_TYPES = ["M", "S", "T"]
     RECIEVE_MESSAGE_TYPES = ["R"]
 
-    def __init__(self, port='/dev/ttyACM0', baudrate=115200, timeout=1):
+    def __init__(self, port='/dev/ttyACM3', baudrate=115200):
         """Instantiate a Communication object.
 
         Parameters:
@@ -16,7 +16,7 @@ class Communication():
             baudrate (int): the baudrate of the serial port. This should match the
                 baudrate set on the arduino.
         """
-        self.arduino = serial.Serial(port=port, baudrate=baudrate, timeout=1)
+        self.arduino = serial.Serial(port=port, baudrate=baudrate, timeout=5)
 
     def send(self, message_type, message_data):
         """Send data to the arduino.
@@ -41,10 +41,17 @@ class Communication():
         Returns:
             (dict): contains message type, processed data, and error value.
         """
-        raw_data = self.arduino.readline()
-        message_type = raw_data[0]
+        raw_data = self.arduino.readline().decode("utf-8") 
+        try:
+            message_type = raw_data[0]
+        except IndexError:
+            return {
+                "message_type": "ERROR",
+                "data": "EMPTY",
+                "error": 2,
+            }
         if message_type in self.RECIEVE_MESSAGE_TYPES:
-            data = raw_data.split["\n"][0].split["\r"][0][1:]
+            data = raw_data.split("\n")[0].split("\r")[0][1:]
             return {
                 "message_type": message_type,
                 "data": data,
@@ -56,12 +63,29 @@ class Communication():
                 "data": raw_data,
                 "error": 1,
             }
+    
+    def send_recieve(self, message_type, message_data):
+        """Send data and expect a response.
+
+        Parameters:
+            message_type (str): the message type. Must be a type listed in the send
+                message type list stored in the communications class.
+            message_data (str): data to be send over the serial bus to the arduino.
+        Returns:
+            (dict): contains message type, processed data, and error value.
+        """
+        self.send(message_type, message_data)
+        return self.receive()
 
 # Systems Test
 if __name__ == "__main__":
     comms = Communication()
-    while True:
-        message = comms.receive()
-        print(type(message))
-        print(message)
-        time.sleep(0.5)
+    stop = False
+    while not stop:
+        resp = comms.send_recieve("M", "<180+180>")
+        print(resp)
+        time.sleep(1)
+        if resp["error"] == 0:
+            stop = True
+        # print(comms.send_recieve("M", "<100+100>"))
+        # time.sleep(1)
--- python/scanviz/scanner.py
+++ python/scanviz/scanner.py
@@ -1,8 +1,10 @@
+from scanviz.communication import Communication
+
 class Scanner():
     """API for interfacing with the scanner"""
 
     def __init__(self):
-        pass
+        self.comms = Communication()
 
     def set_position(self, theta_1, theta_2):
         pass
