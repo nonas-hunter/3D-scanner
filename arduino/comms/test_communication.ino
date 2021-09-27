5d4bb205b2032596ef0a34389daa9c4561476499
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-27 19:22:46 -0400

    Read messages from python

--- arduino/comms/test_communication.ino
+++ arduino/comms/test_communication.ino
@@ -1,15 +1,21 @@
+int state = 0;
+#define TYPE_INDEX 0
+#define DATA_INDEX 1
+
+
+
 void setup() {
  Serial.begin(115200);
+ Serial.flush();
+
 }
+
 void loop() {
-    //for(int num = 0; num <= 1000; num += 1){
-    //    Serial.println(num);
-   
+    char messageType;
+    String data;
     if (!Serial.available()){
-        char inByte = Serial.read();
-        type-data = inByte.substring(stype, etype);
-        data = inByte.substring(sdata, edata);
-        Serial.println(inByte);
-        }
+        String message = Serial.readStringUntil('\n');
+        messageType = message.charAt(TYPE_INDEX);
+        data = message.substring(DATA_INDEX);
     }
 }
