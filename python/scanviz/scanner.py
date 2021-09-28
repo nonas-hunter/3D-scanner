913c2f04856ef20106ec46b07d22e4b0b4ae1bfa
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-28 12:30:21 -0400

    Add functionality to set servo position

--- python/scanviz/scanner.py
+++ python/scanviz/scanner.py
@@ -1,3 +1,5 @@
+import time
+
 from scanviz.communication import Communication
 
 class Scanner():
@@ -6,16 +8,21 @@ class Scanner():
     def __init__(self):
         self.comms = Communication()
 
-    def set_position(self, theta_1, theta_2):
-        if theta_1 > 999 or theta_2 > 999:
+    def set_position(self, pitch, yaw):
+        if pitch > 999 or yaw > 999:
             raise ValueError("Cannot send motor angles greater than 180 deg.")
-        message = f"{theta_1:03d}+{theta_2:03d}"
-        print(message)
-        self.comms.send_recieve("M", message)
+        print(f"Setting servo positions to (pitch) {pitch}, (yaw) {yaw}.")
+        message = f"{pitch:03d}+{yaw:03d}"
+        response = self.comms.send_recieve("M", message)
+        if int(response["data"]) == 0:
+            raise ValueError("Servo did not respond. Stopping program.")
+        else:
+            time.sleep(int(response["data"]))
+        print("Servo positions have been set.")
 
     def get_distance(self):
         pass
 
 if __name__ == "__main__":
     scanner = Scanner()
-    scanner.set_position(1000,1000)
+    scanner.set_position(100,100)
