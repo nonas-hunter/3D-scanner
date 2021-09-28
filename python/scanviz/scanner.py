9e5ce620e32ce19dd985ee11ccc3812c0e344e69
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-28 11:06:50 -0400

    Start writting send motor data function

--- python/scanviz/scanner.py
+++ python/scanviz/scanner.py
@@ -7,7 +7,15 @@ class Scanner():
         self.comms = Communication()
 
     def set_position(self, theta_1, theta_2):
-        pass
+        if theta_1 > 999 or theta_2 > 999:
+            raise ValueError("Cannot send motor angles greater than 180 deg.")
+        message = f"{theta_1:03d}+{theta_2:03d}"
+        print(message)
+        self.comms.send_recieve("M", message)
 
     def get_distance(self):
         pass
+
+if __name__ == "__main__":
+    scanner = Scanner()
+    scanner.set_position(1000,1000)
