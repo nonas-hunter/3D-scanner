e57fc135814381f635bf9a7e2620e7d818a8aa2a
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-09-27 23:04:08 -0400

    Add infrastructure to implement response to python

--- arduino/comms/test_communication.ino
+++ arduino/comms/test_communication.ino
@@ -1,74 +1,41 @@
 int state = 0;
 #define TYPE_INDEX 0
 #define DATA_INDEX 1
-#define M1 1
-#define M2 4
-#define MEND 8
 #include <Servo.h>
 
-//sensor
-int distancesensor = 0;
-int value = 0;
+void sendMessage(char messageType, String messageData) {
+    Serial.print(messageType);
+    Serial.println(messageData);
+}
 
-Servo servo1;  // creates servo 1
-Servo servo2;  // creates servo 2 
-int wait_time = 0;
-
-void sayHi() {
-    Serial.print("yo.");
+void analyzeMessage(String message) {
+    char messageType = message.charAt(TYPE_INDEX);
+    String data = message.substring(DATA_INDEX);
+    switch (messageType) {
+        case 'M':
+            sendMessage('R', "MOTOR");
+            break;
+        case 'S':
+            sendMessage('R', "SENSOR");
+            break;
+        case 'T':
+            sendMessage('T', "12345");
+            break;
+    }
 }
 
 void setup() {
- Serial.begin(115200);
- Serial.flush();
- servo1.attach(10);
- servo2.attach(9);
+    Serial.begin(115200);
+    Serial.flush();
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-    char messageType;
-    String data;
-    if (!Serial.available()){
+    if (Serial.available() > 0) {
+        digitalWrite(LED_BUILTIN, HIGH);
         String message = Serial.readStringUntil('\n');
-        messageType = message.charAt(TYPE_INDEX);
-        data = message.substring(DATA_INDEX);
-    }
-            
-            
-    switch (messageType) {
-        case 'M': {
-            
-            int M1Pos = data.substring(M1, M2).toInt();
-            int M2Pos = data.substring(M2, MEND).toInt();
-            
-            
-            servo1.write(M1Pos);
-            servo2.write(M2Pos);
-            //Serial.print(M1Pos);
-            //Serial.print("+");
-            //Serial.println(M2Pos);
-            delay(1000);
-            value = analogRead(distancesensor);
-            Serial.print('R');
-            Serial.print(M1Pos);
-            Serial.print(",");
-            Serial.print(M2Pos);
-            Serial.print(",");
-            Serial.println(value);
-        }
-            break;
-        case 'S':
-            {
-            //value = analogRead(distancesensor);
-            Serial.print('R');
-            Serial.println("HELLo");
-            }
-            break;
-        default:
-            {
-            Serial.print('R');
-            Serial.println(messageType);
-            }
-            break;
+    } else {
+        digitalWrite(LED_BUILTIN, LOW);
     }
 }
